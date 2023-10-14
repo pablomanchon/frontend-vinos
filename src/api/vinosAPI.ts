102b05a3916b90648687f89f13f1b9439ffbc3fc
Pablo Manchon <pablomanchon97@gmail.com>
2023-10-14 16:17:16 -0300

    cambio back

--- src/api/vinosAPI.ts
+++ src/api/vinosAPI.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { Vino } from "../hooks/types";
 
-const api = axios.create({ baseURL: "https://backend-vinos.onrender.com" });
+const api = axios.create({
+  baseURL: "https://backend-vinos-production.up.railway.app",
+});
 
 export const getVinos = async (): Promise<Vino[]> => {
   const response = await api.get("/vinos").catch((err) => {
